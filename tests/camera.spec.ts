f70010a92b3b5baa624d67bc2e7fc791afe673f0
Matt Davis <matt@objectiveplay.com>
2026-06-23 13:35:37 -0600

    update to vector type

--- tests/camera.spec.ts
+++ tests/camera.spec.ts
@@ -1,17 +1,17 @@
 
 import { Location } from '../engine/unreal-world';
-import { test, expect } from '@specter/test'
+import { test, expect, Vector3 } from '@specter/test'
 import { GameAssets } from '../support/assets';
 
 test.describe.configure({ mode: "serial" })
 
-test('Camera auto tracks players', async ({ world, page }) => {
+test('Camera auto tracks players', async ({ world }) => {
 
-    const spawnLocations: Location[] = [
-        { X: 1000, Y: -1000, Z: 0 },
-        { X: 1000, Y: 1000, Z: 0 },
-        { X: -1000, Y: 1000, Z: 0 },
-        { X: -1000, Y: -1000, Z: 0 },
+    const spawnLocations: Vector3[] = [
+        { x: 1000, y: -1000, z: 0 },
+        { x: 1000, y: 1000, z: 0 },
+        { x: -1000, y: 1000, z: 0 },
+        { x: -1000, y: -1000, z: 0 },
     ]
 
     for (const location of spawnLocations) {
@@ -25,7 +25,7 @@ test('Players death removes camera tracking after three seconds', async ({ world
     const marshmallow1 = await world.spawnActor(GameAssets.Characters.Marshmallow, { location: { X: 500, Y: 0, Z: 0 } })
     const marshmallow2 = await world.spawnActor(GameAssets.Characters.Marshmallow, { location: { X: -500, Y: 0, Z: 0 } })
 
-    await marshmallow2.applyEffect(GameAssets.Effects.Abyss)
+    await marshmallow2.applyGameplayEffect(GameAssets.Effects.Abyss)
 
     await expect(marshmallow1).toBeVisible({ onScreen: true })
     await expect(marshmallow2).not.toBeVisible({ onScreen: true })
